ccafb27cc166956edb6beb9015d3a1da26bd5b2b
Manya1892005 <tewarimanya18@gmail.com>
2026-09-20 23:22:53 +0530

    Solve Two Sum problem

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+def two_sum(nums, target):
+    seen = {}
+
+    for i, x in enumerate(nums):
+        complement = target - x
+
+        if complement in seen:
+            return [seen[complement], i]
+
+        seen[x] = i
