a39f49b124399a534906bf90672fc60fb1efe732
Manya1892005 <tewarimanya18@gmail.com>
2026-09-21 14:32:19 +0530

    Solve the best time to buy and sell stock problem

---
+++ best time to buy and sell stock.py
@@ -0,0 +1,14 @@
+def max_profit(prices):
+    min_price = prices[0]
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+
+        profit = price - min_price
+
+        if profit > max_profit:
+            max_profit = profit
+
+    return max_profit
